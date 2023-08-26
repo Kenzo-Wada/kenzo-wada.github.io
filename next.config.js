d45ef10b8e66e4878582377a0a55ea6f2ea774ad
Kenzo Wada <used.acormhib@gmail.com>
2023-08-26 22:59:02 +0900

    :bug: bug fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
