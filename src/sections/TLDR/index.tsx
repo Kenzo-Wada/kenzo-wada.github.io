df89584a3e57b64b1443003c9a1326b39730396b
Kenzo-Wada <used.acormhib@gmail.com>
2023-11-05 23:55:47 +0900

    bug fix

--- src/sections/TLDR/index.tsx
+++ src/sections/TLDR/index.tsx
@@ -6,7 +6,7 @@ const Tldr = () => {
   return (
     <SectionLayout title="TL;DR" id="tldr">
       <Flex direction="column" gap="3">
-        <Text size="6">- I'm a frontend engineer who loves modern technologies.</Text>
+        <Text size="6">- A frontend engineer who loves modern technologies.</Text>
         <Text size="6">- I can create a product with optimized logic and readable code using Next.js.</Text>
         <Text size="6">
           - Three years of engineering experience, fresh out of college in my first year. Super young, but skilled.
